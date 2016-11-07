23abbf280403edb1aef9c6c1109016b5cdbf3b65
Eugene Istomin <e.istomin@cone.ee>
2016-11-07 11:42:51 +0200

    dracut

--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/_tmp/build_docker_to_vm.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/_tmp/build_docker_to_vm.sh
@@ -10,15 +10,7 @@ systemctl enable rev5_init_auto_xen
  ### 
  
  ###  CP REV5 - BOOT, GRUB2###
-cp /media/sysdata/cho/cho_v5/in4_landscape/internals--c/linux_sys--o/boot--f/grub2/dsl/main--s/simple/files/hardcoded/etc_default_grub--vm_xen ./loop/etc/default/grub
-cp /media/sysdata/cho/cho_v5/in4_landscape/internals--c/linux_sys--o/boot--f/dracut/dsl/main--s/simple/files/hardcoded/dracut.conf-vm ./loop/etc/dracut.conf
-#cp /media/sysdata/cho/cho_v5/in4_landscape/internals--c/linux_sys--o/boot--f/dracut/dsl/main--s/simple/files/hardcoded/51-wa_xenvm_blacklist.conf ./loop/etc/modprobe.d/
-#Temp - not in svn
-#echo "blacklist xen-blkfront" >  ./loop/etc/modprobe.d/51-block-xenvm_blacklist.conf
 
-
-mkdir -p  ./loop/boot/grub2/
-cp /media/sysdata/cho/cho_v5/in4_landscape/internals--c/linux_sys--o/boot--f/grub2/dsl/main--s/simple/files/hardcoded/boot_grub2_grub.cfg--xen-all ./loop/boot/grub2/grub.cfg
 
 ## ADD MISC SW
 #zypper
--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/repo_and_packages.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/repo_and_packages.sh
@@ -22,11 +22,14 @@ zypper --non-interactive in --force hostname iproute2 wicked-service dbus-1 SuSE
 zypper --non-interactive in --force bash-completion systemd-bash-completion aaa_base-extras sudo man man-pages
 zypper --non-interactive in --force openssh
 zypper --non-interactive in --force rsyslog rsyslog-module-relp rsyslog-module-mmnormalize 
-zypper --non-interactive in --force btrfsprogs e2fsprogs sysfsutils
+zypper --non-interactive in --force btrfsprogs e2fsprogs sysfsutils quota
 zypper --non-interactive in --force sssd sssd-tools 
 zypper --non-interactive in --force yast2-security
-zypper --non-interactive in --force mc vim lsof less strace 
+zypper --non-interactive in --force mc vim lsof less strace pwgen 
+zypper --non-interactive in --force bzip2 gzip p7zip unzip zip tar 
 #MISC
-zypper --non-interactive in --force rsync subversion git sysstat tcpdump telnet wget
-#curl deltarpm expect sysfsutils exim atop diff
+zypper --non-interactive in --force rsync subversion git sysstat tcpdump telnet wget mailx
+zypper --non-interactive in --force exim curl expect  deltarpm
+#atop drbd-kmp-default 
+#+ pam + policy*
 zypper --non-interactive --gpg-auto-import-keys dup
