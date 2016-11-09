df44f29c864b52b7ff37f0741d8147ea40059449
Eugene Istomin <e.istomin@cone.ee>
2016-11-09 18:50:25 +0200

    step2

--- cho_v4/services--c/database--o/rdbms--f/oracle10g--g/migration2in4a1.sh
+++ cho_v4/services--c/database--o/rdbms--f/oracle10g--g/migration2in4a1.sh
@@ -5,8 +5,8 @@ mkfs.btrfs  -L "storage" ./storage.raw
 ###
 
 ### TMP ###
-zypper ar -cf http://download.opensuse.org/repositories/filesystems/openSUSE_Leap_42.1/filesystems.repo
-zypper dup
+zypper ar -cf http://download.opensuse.org/repositories/filesystems/openSUSE_Leap_42.2/filesystems.repo
+zypper --gpg-auto-import-keys dup
 ###
 
 ### MOUNTS ###
@@ -20,6 +20,13 @@ mkfs.btrfs  -L "storage" /dev/sdd
 mount /dev/disk/by-label/storage -o nodatacow,noatime,autodefrag,recovery /media/storage
 # ?? skip_balance
 ###
+
+
+### ZYPPER  ###
+ zypper --non-interactive in unixODBC unixODBC-devel bison flex gcc make gcc-c++ pcre-devel zlib-devel patch m4 glibc binutils glibc-devel libaio1 libaio-devel libelf0 libelf1 libelf-devel numactl libtool libstdc++6 libstdc++-devel libgcc_s1 expat libopenssl-devel binutils-devel glibc-devel-32bit libaio-devel-32bit unixODBC-32bit libgthread-2_0-0-32bit gcc-32bit gcc-c++-32bit gcc48-32bit libgcc_s1-32bit glibc-32bit binutils-devel-32bit
+ zypper --non-interactive in http://ftp.novell.com/partners/oracle/orarun-2.0-1.4.0.x86_64.rpm
+###
+
 
 ### AS INIT  ###
 mkdir -p /media/storage/as
@@ -69,17 +76,13 @@ setfacl -m o::rx  /media/storage/ts
 ###
 
 
-### ORACLE INIT  ###
-rm -f /etc/systemd/system/in4__oracle10g.service 	&& cp  /media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms:f/oracle10g:g/_systemd/in4__oracle10g.service /etc/systemd/system/
-rm -f  /etc/profile.d/oracle.sh && ln -s /media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms:f/oracle10g:g/init/profile.d_oracle.sh /etc/profile.d/oracle.sh
+### ORACLE INIT  - /etc ###
+rm -f /etc/systemd/system/in4__oracle10g.service 	&& cp  /media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms--f/oracle10g--g/_systemd/in4__oracle10g.service /etc/systemd/system/
+rm -f  /etc/profile.d/oracle.sh && ln -s /media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms--f/oracle10g--g/init/profile.d_oracle.sh /etc/profile.d/oracle.sh
+touch /etc/oratab && chown oracle:oinstall /etc/oratab && chmod 750 /etc/oratab
 ###
 
 
-###  ###
- zypper --non-interactive in unixODBC unixODBC-devel bison flex gcc make gcc-c++ pcre-devel zlib-devel patch m4 glibc binutils glibc-devel libaio1 libaio-devel libelf0 libelf1 libelf-devel numactl libtool libstdc++6 libstdc++-devel libgcc_s1 expat libopenssl-devel binutils-devel glibc-devel-32bit libaio-devel-32bit unixODBC-32bit libgthread-2_0-0-32bit gcc-32bit gcc-c++-32bit gcc48-32bit libgcc_s1-32bit glibc-32bit binutils-devel-32bit
- zypper --non-interactive in http://ftp.novell.com/partners/oracle/orarun-2.0-1.4.0.x86_64.rpm
-
-###
 
 ###  ??  ###
 usermod -s /bin/bash oracle 
@@ -96,49 +99,35 @@ echo "umask 022" > /opt/oracle/.bash_profile
 ###
 
 
-###  ###
+### OLD ORACLE -> MIGRATE ###
 mkdir -p /media/storage_old/_tmp/ && cd /media/storage_old/_tmp/
 wget http://public.edss.ee/software/Linux/Oracle/oracle10.2.0.5EE_rev2.tar.gz
 tar -xzf ./oracle10.2.0.5EE_rev2.tar.gz&
 cp -R /media/storage_old/_tmp/software/oracle/product /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/
-chown -R oracle:oinstall /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s
-mkdir /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/in4/
-cp /media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms:f/oracle10g:g/init/* /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/in4/
- touch /etc/oratab && chown oracle:oinstall /etc/oratab && chmod 750 /etc/oratab
+#mkdir /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/in4/
+#cp /media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms--f/oracle10g--g/init/* /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/in4/
+ chown -R oracle:oinstall /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s
 ###
 
 ### LOGGING SYMLINKS  ###
-mkdir /media/storage/as/oracle/dumps/adump
-mkdir /media/storage/as/oracle/dumps/bdump
-mkdir /media/storage/as/oracle/dumps/cdump
-mkdir /media/storage/as/oracle/dumps/udump
-mkdir /media/storage/as/oracle/dumps/
-mkdir /media/storage/as/oracle/dumps/
+.
 
 mkdir /media/storage/as/oracle/logs/rdbms /media/storage/as/oracle/logs/network
 
 ln -s /media/storage/as/oracle/logs/startup.log /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/startup.log
 ln -s /media/storage/as/oracle/logs/shutdown.log /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/shutdown.log
 
-rm -rf /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/log && ln -s /media/storage/as/oracle/logs/rdbms /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/log
-rm -f /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/audit && ln -s /media/storage/as/oracle/logs/rdbms /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/audit
+rm -rf /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/rdbms/log && ln -s /media/storage/as/oracle/logs/rdbms /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/rdbms/log
+rm -rf /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/rdbms/audit && ln -s /media/storage/as/oracle/logs/rdbms /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/rdbms/audit
 
 rm -rf /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/network/log && ln -s  /media/storage/as/oracle/logs/network /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/network/log
 rm -rf /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/network/trace && ln -s  /media/storage/as/oracle/logs/network /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/network/trace
 
+ rm -rf /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/dbs &&  ln -s /media/storage/as/oracle/sid /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/dbs
 ### 
 
 
 ###  ###
-
-setfacl -R -m d:u:oracle:rwx /media/storage/database
-setfacl -R -m d:g:oinstall:rwx /media/storage/database
-setfacl -R -m u:oracle:rwx /media/storage/database
-setfacl -R -m g:oinstall:rwx /media/storage/database
-setfacl -R -m d:u:oracle:rwx /media/storage/software
-setfacl -R -m d:g:oinstall:rwx /media/storage/software
-setfacl -R -m u:oracle:rwx /media/storage/software
-setfacl -R -m g:oinstall:rwx /media/storage/software
 ###
 
 
@@ -153,10 +142,11 @@ setfacl -R -m g:oinstall:rwx /media/storage/software
 ###  ###
 ###
 
+chown -R oracle:oinstall /media/storage/as/oracle
 
 ### CREATE RO SNAPSHOT  ###
 btrfs subvolume delete /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g
-btrfs subvolume snapshot -r  /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g
+btrfs subvolume snapshot -r /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g
 ###
 
 ####
@@ -175,10 +165,6 @@ mkdir -p /media/storage/snapshots/as/oracle/
 /media/storage/as/oracle/snapshots/data/weekly/`date +%d.%m.%y_%H:%M:%S`
 ###
 
-### _tmp ###
- rm -rf /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/dbs &&  ln -s /media/storage/as/oracle/sid /media/storage/ts/services--c/database--o/rdbms--f/oracle10g--g_rw/ee--s/product/10g/dbs
-
-###
 
 
 ALC_PATH_ARR=`echo $ALC_PATH`
@@ -186,3 +172,19 @@ ALC_PATH_ARR=`echo $ALC_PATH`
         setfacl -m g::rx $PATH
         setfacl -m o::rx $PATH
     done
+
+    The following 8 packages are going to be downgraded:
+  bash bash-completion ksh libreadline6 systemd systemd-sysvinit udev util-linux
+
+The following 8 packages are going to change vendor:
+  bash              obs://build.opensuse.org/shells -> openSUSE         
+  bash-completion   obs://build.opensuse.org/shells -> openSUSE         
+  ksh               obs://build.opensuse.org/shells -> openSUSE         
+  libreadline6      obs://build.opensuse.org/shells -> openSUSE         
+  systemd           obs://build.opensuse.org/home:conecenter -> openSUSE
+  systemd-sysvinit  obs://build.opensuse.org/home:conecenter -> openSUSE
+  udev              obs://build.opensuse.org/home:conecenter -> openSUSE
+  util-linux        obs://build.opensuse.org/home:conecenter -> openSUSE
+
+    
+
