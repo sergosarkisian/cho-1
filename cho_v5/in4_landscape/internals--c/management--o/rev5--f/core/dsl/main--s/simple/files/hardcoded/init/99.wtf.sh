2909d8c7c500b3c0dd263765c18ba4aff74ff58d
Eugene Istomin <e.istomin@cone.ee>
2016-11-09 23:27:35 +0200

    step2

--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/99.wtf.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/99.wtf.sh
@@ -9,3 +9,8 @@ mount -o remount /etc /etc
 mount -o remount /usr /usr
 
 sed -i "s/set term xy/#set term xy/" /etc/inputrc 
+
+cp /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/wtf/wickedd-dhcp /usr/lib/systemd/system/wickedd-auto4.service
+cp /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/wtf/wickedd-dhcp /usr/lib/systemd/system/wickedd-dhcp4.service
+systemctl disable wickedd-dhcp6
+systemctl mask wickedd-dhcp6
