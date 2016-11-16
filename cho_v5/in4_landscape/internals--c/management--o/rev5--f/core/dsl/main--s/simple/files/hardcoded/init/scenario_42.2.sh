3e1a044d8dbecd015d86d6accd5986e932aeee97
Eugene Istomin <e.istomin@cone.ee>
2016-11-16 22:21:18 +0200

    step2

--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario_42.2.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario_42.2.sh
@@ -2,9 +2,9 @@
 
 sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/2.all-repo_42.2.sh
 sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/2.all-packages.sh
-if [[ $TYPE == "vm" or $TYPE == "hw" ]]; then sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/2.kern-packages.sh; fi
+if [[ $TYPE == "vm" ]] || [[ $TYPE == "hw" ]]; then sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/2.kern-packages.sh; fi
 if [[ $TYPE == "vm" ]]; then sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/2.vm-packages; fi
-if [[ $TYPE == "vm" or $TYPE == "hw" ]]; then sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/3.kern-step2_boot.sh; fi
+if [[ $TYPE == "vm" ]] || [[ $TYPE == "hw" ]]; then sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/3.kern-step2_boot.sh; fi
 sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/4.docker2vm_boot.sh
 sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/5.all-pre.sh
 sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/6.all-conf_init.sh
