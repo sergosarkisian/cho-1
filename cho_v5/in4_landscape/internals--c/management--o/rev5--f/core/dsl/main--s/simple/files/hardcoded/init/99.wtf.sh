686b9b3f698574e97de3fcb4d93d6bd88bb6fc85
Eugene Istomin <e.istomin@cone.ee>
2016-11-17 16:07:46 +0200

    step2

--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/99.wtf.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/99.wtf.sh
@@ -1,14 +1,14 @@
 # systemctl daemon-reexec
-# umount /media/sysdata/linux_sys/home 
-# umount /media/sysdata/linux_sys/home 
-# umount /media/sysdata/linux_sys/root
-# umount /media/sysdata/linux_sys/root
+umount /media/sysdata/linux_sys/home 
+umount /media/sysdata/linux_sys/home 
+umount /media/sysdata/linux_sys/root
+umount /media/sysdata/linux_sys/root
 # 
-# #mount -o remount /boot /boot
-# #mount -o remount /etc /etc
-# mount -o remount /usr /usr
+mount -o remount /boot /boot
+mount -o remount /etc /etc
+mount -o remount /usr /usr
 # 
-# sed -i "s/set term xy/#set term xy/" /etc/inputrc 
+sed -i "s/set term xy/#set term xy/" /etc/inputrc 
 # 
 # 
 # 
