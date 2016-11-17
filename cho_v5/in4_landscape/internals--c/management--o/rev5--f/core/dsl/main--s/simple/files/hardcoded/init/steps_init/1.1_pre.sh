16234586d79b825e6492a1be9574d118ca474162
Eugene Istomin <e.istomin@cone.ee>
2016-11-17 21:34:02 +0200

    step2

--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/1.1_pre.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/1.1_pre.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 echo -e "\n\n######## ######## BEGIN -  steps_init - `echo ${BASH_SOURCE[0]}|awk -F/ '{print $NF}'` ######## ########\n\n"
 
 # ### 42.1 ###
--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/2.post.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/2.post.sh
@@ -4,12 +4,12 @@ echo -e "\n\n######## ######## BEGIN -  steps_init - `echo ${BASH_SOURCE[0]}|awk
 
 . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/build_os-vm_env.sh
 
-umount $BUILD_ENV/loop/dev
-umount $BUILD_ENV/loop/proc 
-umount $BUILD_ENV/loop/sys
+if [[ -d $BUILD_ENV/loop/dev ]]; then umount $BUILD_ENV/loop/dev; fi
+if [[ -d $BUILD_ENV/loop/proc ]]; then umount $BUILD_ENV/loop/proc; fi
+if [[ -d $BUILD_ENV/loop/sys ]]; then umount $BUILD_ENV/loop/sys; fi
 
-umount $BUILD_ENV/loop/media/sysdata 
-umount $BUILD_ENV/loop 
+if [[ -d $BUILD_ENV/loop/media/sysdata ]]; then umount $BUILD_ENV/loop/media/sysdata; fi
+if [[ -d $BUILD_ENV/loop/media ]]; then umount $BUILD_ENV/loop; fi
 
 losetup -d /dev/$LO_SYSDATA
 losetup -d /dev/$LO_SYSTEM
