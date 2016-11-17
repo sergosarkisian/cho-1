9cfbaf2668d091e8c182ec06ce8d51e48c43a62f
Eugene Istomin <e.istomin@cone.ee>
2016-11-17 12:40:45 +0200

    step2

--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario_42.2.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario_42.2.sh
@@ -5,7 +5,6 @@
 if [[ $TYPE == "vm" ]] || [[ $TYPE == "hw" ]]; then . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/2.kern-packages.sh; fi
 if [[ $TYPE == "vm" ]]; then . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/2.vm-packages; fi
 if [[ $TYPE == "vm" ]] || [[ $TYPE == "hw" ]]; then . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/3.kern-step2_boot.sh; fi
-. /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/4.docker2vm_boot.sh
 . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/5.all-pre.sh
 . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/6.all-conf_init.sh
 . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/scenario/7.all-common.sh
