e419978b5f5fc084888d3cff46f40f062b26c017
Eugene Istomin <e.istomin@cone.ee>
2016-11-17 15:11:54 +0200

    step2

--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/1.1_pre.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/1.1_pre.sh
@@ -11,7 +11,7 @@ echo -e "\n\n######## ######## START -  steps_init - ${0##*/} ######## ########\
 . /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/2.post.sh
 
 ### OPENSUSE INIT
-zypper --non-interactive in --force wget btrfsprogs parted git
+zypper --non-interactive in --force wget btrfsprogs parted git xz tar
 #
 
 ### 42.2 ###
--- cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/1.3_exec.sh
+++ cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/steps_init/1.3_exec.sh
@@ -20,7 +20,7 @@ chmod 744  $BUILD_ENV/loop/etc/sysconfig/
 
  ###  CHROOT TO LOOP ###
 mount -t proc proc $BUILD_ENV/loop/proc/ &&  mount -t sysfs sys $BUILD_ENV/loop/sys/ && mount -o bind /dev $BUILD_ENV/loop/dev/
-chroot $BUILD_ENV/loop /bin/bash -c "export TYPE=$TYPE; sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/$SCENARIO"
+chroot $BUILD_ENV/loop /bin/bash -c "export TYPE=$TYPE; export LO_SYSTEM=$LO_SYSTEM; sh /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/rev5--f/core/dsl/main--s/simple/files/hardcoded/init/$SCENARIO"
 ###
 
 echo -e "\n\n######## ######## STOP -  steps_init - ${0##*/} ######## ########\n\n"
