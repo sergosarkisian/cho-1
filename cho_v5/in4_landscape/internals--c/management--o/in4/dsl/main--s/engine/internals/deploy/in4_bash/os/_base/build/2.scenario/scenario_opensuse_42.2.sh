d54eae125b84ff70ef657d6d928064c03964fc48
Eugene Istomin <e.istomin@cone.ee>
2016-11-20 21:49:50 +0200

    step3

--- cho_v5/in4_landscape/internals--c/management--o/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_base/build/2.scenario/scenario_opensuse_42.2.sh
+++ cho_v5/in4_landscape/internals--c/management--o/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_base/build/2.scenario/scenario_opensuse_42.2.sh
@@ -17,7 +17,7 @@
     zypper --non-interactive in --force util-linux-systemd    
 else
     echo "Offline mode"
-#     rm -rf /etc/zypp/repos.d/*    
+    rm -rf /etc/zypp/repos.d/*    
     zypper --non-interactive --no-remote in /tmp/*.rpm    
 fi
 
