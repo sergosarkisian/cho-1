aaa7a25c07bc4239d93d8c8be77f3754da59d113
Eugene Istomin <e.istomin@cone.ee>
2016-11-20 12:51:36 +0200

    step3

--- cho_v5/in4_landscape/internals--c/management--o/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_base/build/2.scenario/scenario_opensuse_42.2.sh
+++ cho_v5/in4_landscape/internals--c/management--o/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_base/build/2.scenario/scenario_opensuse_42.2.sh
@@ -29,19 +29,19 @@ LogMsg="BEGIN -  steps_init - $ExecScriptname"
 echo -e "\n\n########  $LogMsg  ########\n\n"; logger -p info -t "in4" $LogMsg
 ###
 
-. $In4_Exec_Path/2.scenario/2.all-repo_42.2.sh
-. $In4_Exec_Path/2.scenario/2.all-packages.sh
-if [[ $DeployOsMode== "vm_xen" ]] || [[ $DeployOsMode== "hw_chroot" ]]; then . $In4_Exec_Path/2.scenario/2.kern-packages.sh; fi
+. $In4_Exec_Path/_base/2.scenario/2.all-repo_42.2.sh
+. $In4_Exec_Path/_base/2.scenario/2.all-packages.sh
+if [[ $DeployOsMode== "vm_xen" ]] || [[ $DeployOsMode== "hw_chroot" ]]; then . $In4_Exec_Path/_base/2.scenario/2.kern-packages.sh; fi
 if [[ $DeployOsMode== "vm_xen" ]]; then . $In4_Exec_Path/_sub/vm_xen/2.scenario/2.vm_xen-packages.sh; fi
 if [[ $DeployOsMode== "hw_chroot" ]]; then . $In4_Exec_Path/_sub/hw_chroot/2.scenario/2.hw_chroot-packages.sh; fi
-if [[ $DeployOsMode== "vm_xen" ]] || [[ $DeployOsMode== "hw_chroot" ]]; then . $In4_Exec_Path/2.scenario/3.kern-step2_boot.sh; fi
-. $In4_Exec_Path/2.scenario/5.all-pre.sh
-. $In4_Exec_Path/2.scenario/6.all-conf_init.sh
-## . $In4_Exec_Path/2.scenario/7.all-common.sh ## BUG
-. $In4_Exec_Path/2.scenario/8.mounts.sh
+if [[ $DeployOsMode== "vm_xen" ]] || [[ $DeployOsMode== "hw_chroot" ]]; then . $In4_Exec_Path/_base/2.scenario/3.kern-step2_boot.sh; fi
+. $In4_Exec_Path/_base/2.scenario/5.all-pre.sh
+. $In4_Exec_Path/_base/2.scenario/6.all-conf_init.sh
+## . $In4_Exec_Path/_base/2.scenario/7.all-common.sh ## BUG
+. $In4_Exec_Path/_base/2.scenario/8.mounts.sh
 if [[ $DeployOsMode== "vm_xen" ]]; then . $In4_Exec_Path/_sub/vm_xen/2.scenario/9.vm_xen_post.sh; fi
 if [[ $DeployOsMode== "hw_chroot" ]]; then . $In4_Exec_Path/_sub/vm_xen/2.scenario/9.hw_chroot_post.sh; fi
-. $In4_Exec_Path/2.scenario/10.clean.sh
+. $In4_Exec_Path/_base/2.scenario/10.clean.sh
 
 ### IN4 BASH FOOTER ###
 CurDirPath=`echo ${BASH_SOURCE[0]}|sed "s/4//"`; ExecScriptname=`echo ${BASH_SOURCE[0]}`
