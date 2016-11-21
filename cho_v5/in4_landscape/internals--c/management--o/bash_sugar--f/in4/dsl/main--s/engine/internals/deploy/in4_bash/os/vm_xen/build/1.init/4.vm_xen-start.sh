50e2fda9bc0a8e2e56a78743780681c8dc485ce2
Eugene Istomin <e.istomin@cone.ee>
2016-11-21 02:06:05 +0200

    step3

--- cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/vm_xen/build/1.init/4.vm_xen-start.sh
+++ cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/vm_xen/build/1.init/4.vm_xen-start.sh
@@ -9,17 +9,17 @@ cp --sparse=always $BUILD_ENV/swap.raw $BUILD_ENV/../
 cp /media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_sub/vm_xen/3.env/demo.xl  /tmp/demo.xl
 BUILD_ENV_ESC=$(echo "$BUILD_ENV" | sed 's/\//\\\//g')
 sed -i "s/VMPATH/$BUILD_ENV_ESC/g" /tmp/demo.xl
-xl create /tmp/demo.xl
-DOMID=`xl domid demo-hvxen-test`
+sudo xl create /tmp/demo.xl
+DOMID=`sudo xl domid demo-hvxen-test`
 
 secs=60  
 SECONDS=0 
 while (( SECONDS < secs )); do    # Loop until interval has elapsed.
-    STATE=`xenstore-read /local/domain/$DOMID/device/vif/0/state`
+    STATE=`sudo xenstore-read /local/domain/$DOMID/device/vif/0/state`
     if [[ $STATE == 4 ]]; then break; fi
-    sleep 1
+    sleep 5
 done
-xl destroy demo-hvxen-test
+sudo xl destroy demo-hvxen-test
 
 if [[ $STATE != 4 ]]; then exit 1; fi
 
--- cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/vm_xen/build/2.scenario/9.vm_xen-post.sh
+++ cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/vm_xen/build/2.scenario/9.vm_xen-post.sh
@@ -4,8 +4,8 @@ echo -e "\n\n######## ######## BEGIN -  steps_init - `echo ${BASH_SOURCE[0]}|awk
 
 ### XEN DOMU AUTONAMING  ###
 #SYSTEMD
- rm -f  /etc/systemd/system/init_auto_xen.service && cp/media/sysdata/in4/cho/cho_v5/in4_landscape/internals--c/management--o/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_sub/vm_xen/3.env/init_auto_xen.service /etc/systemd/system/
-systemctl enable init_auto_xen
+in4func_systemd "internals--c--management--o--bash_sugar--f--in4--g--main--s" "add" "service" "init_auto_xen"
+in4func_systemd "internals--c--management--o--bash_sugar--f--in4--g--main--s" "enable" "service" "init_auto_xen"
 ###
 
 echo -e "\n\n######## ######## END -  steps_init - `echo ${BASH_SOURCE[0]}|awk -F/ '{print $NF}'` ######## ########\n\n"
