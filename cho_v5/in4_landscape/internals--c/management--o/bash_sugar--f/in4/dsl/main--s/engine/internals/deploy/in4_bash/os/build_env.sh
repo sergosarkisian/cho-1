487cbb8651e38ea7638ea823e9163492dfecf5ef
Eugene Istomin <e.istomin@cone.ee>
2016-11-22 10:06:02 +0200

    step3

--- cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/build_env.sh
+++ cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/build_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 VmDiskLoopSystem="loop60"
 VmDiskLoopSysdata="loop61"
-VmDiskSizeSystem="10000m"
-VmDiskSizeSysdata="1000m"
+VmDiskSizeSystem="1500m"
+VmDiskSizeSysdata="400m"
 VmDiskSizeSwap="50m"
 OsBuildScenario="scenario_opensuse_42.2.sh"
 DeployOsArch="x86_64"
