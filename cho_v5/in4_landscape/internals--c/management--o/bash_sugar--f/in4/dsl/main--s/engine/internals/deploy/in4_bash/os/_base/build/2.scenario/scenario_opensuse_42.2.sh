6a9a04f947c4b7c3d6bc12587840778df5ada1cd
Eugene Istomin <e.istomin@cone.ee>
2016-12-08 05:22:51 +0200

    step2

--- cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_base/build/2.scenario/scenario_opensuse_42.2.sh
+++ cho_v5/in4_landscape/internals--c/management--o/bash_sugar--f/in4/dsl/main--s/engine/internals/deploy/in4_bash/os/_base/build/2.scenario/scenario_opensuse_42.2.sh
@@ -30,9 +30,9 @@ if [[ $DeployOsMode == "vm_xen" ]]; then . $In4_Exec_Path/vm_xen/build/2.scenari
 if [[ $DeployOsMode == "hw_chroot" ]]; then . $In4_Exec_Path/hw_chroot/build/2.scenario/2.hw_chroot-packages.sh; fi
 if [[ $DeployOsMode == "vm_xen" ]] || [[ $DeployOsMode == "hw_chroot" ]]; then . $In4_Exec_Path/_base/build/2.scenario/3.kern-step2_boot.sh; fi
 . $In4_Exec_Path/_base/build/2.scenario/5.all-pre.sh
-#. $In4_Exec_Path/_base/build/2.scenario/6.all-conf_init.sh
-#. $In4_Exec_Path/_base/build/2.scenario/7.all-common.sh
-#. $In4_Exec_Path/_base/build/2.scenario/8.mounts.sh
+. $In4_Exec_Path/_base/build/2.scenario/6.all-conf_init.sh
+. $In4_Exec_Path/_base/build/2.scenario/7.all-common.sh
+. $In4_Exec_Path/_base/build/2.scenario/8.mounts.sh
 if [[ $DeployOsMode == "vm_xen" ]]; then . $In4_Exec_Path/vm_xen/build/2.scenario/9.vm_xen-post.sh; fi
 if [[ $DeployOsMode == "hw_chroot" ]]; then . $In4_Exec_Path/hw_chroot/build/2.scenario/9.hw_chroot-post.sh; fi
 . $In4_Exec_Path/_base/build/2.scenario/10.clean.sh
