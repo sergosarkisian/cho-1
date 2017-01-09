3cc45b03857563fff96cac531ebc5f2710cf3ca7
Eugene Istomin <e.istomin@cone.ee>
2017-01-10 01:52:14 +0200

    step3

--- cho_v4/services--c/database--o/rdbms--f/oracle10g--g/sql/simpletest.sh
+++ cho_v4/services--c/database--o/rdbms--f/oracle10g--g/sql/simpletest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 /usr/bin/sleep 30
-sqlplus -l "/ as sysdba" @/media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms--f/oracle10g--g/sql/simpletest.sql
+/media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms--f/oracle10g--g/sqlplus -l "/ as sysdba" @/media/sysdata/in4/cho/cho_v4/services--c/database--o/rdbms--f/oracle10g--g/sql/simpletest.sql
